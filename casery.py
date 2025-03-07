448a111a9379ac7c81992ceae19c966a43a57d0b
aesaraesaraesar <aesar12345a@gmail.com>
2025-03-07 20:08:07 +0300

    Add files via upload

---
+++ casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["C_HRr"]
+casery = "C_HRr"
+##ايدي مطور السورس
+caserid = 6264438859
+##قناه السورس
+source = "https://t.me/HELLASUserBot" 
+ch = "HELLASUserBot" 
+##جروب السورس
+group = "https://t.me/helashelb" 
+##لوجو السورس
+photosource = "https://telegra.ph/file/79d346793ec3e73d5e9f0.jpg"
+##توكن الصانع
+bot_token="6850571274:AAEceZGa8v-39hn7Z6Ov5sSZjiZ_Xey-SF4"
+##جلسه الاشتراك
+bot_token2="BAGhJBcAd_q04EouqPn8tvWLuAQOZDv-b1JVhvRIf-dSZWOa9T-myLNlOhnEPSwsXrrt2D7_OaJfeqlImrCUTPaLwr04Po5ZgHdFWRcQ3E54p4LRgvL5n_T5yxlxKrOhTksHOc-eIzUAKXRnuDkkhH5LTwFJc1QAW9uOU-VSnsXQ6jw3p_8Nhwid3O4B368uwEO84deDsEgMVD_WpW2PhXCr2pa1FMQgK1GK-QYcJQxosOsaOHE2eSKTySMfOXAZr9r76Be26K6OQrMdJAL07OcLa6raIW9lafirCIcVkeaavGg5afbftTtj4bDOjU_8UApOfHQ_BcZXxVMx1c7O-PDgO7um0AAAAAGjNXZeAA"
